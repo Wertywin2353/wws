b71331c06dac1a36ac052411de8cdf9f581805c8
Wertywin2353 <93203329+Wertywin2353@users.noreply.github.com>
2024-10-12 10:37:47 +0300

    Project created -_-

---
+++ js/about.js
@@ -0,0 +1,46 @@
+let text1 = document.getElementById('v1');
+let text2 = document.getElementById('v2');
+let text3 = document.getElementById('v3');
+
+window.onload = function () {
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+        text2.innerHTML = "";
+    }
+    text1.style.translate = "-0% -50%";
+    setTimeout(
+        function () {
+            text2.style.translate = "-50% -50%";
+            endAnim();
+        }, 500
+    );
+}
+
+function endAnim() {
+    setTimeout(
+        function () {
+            text3.style.translate = "-0% -50%";
+            fadeOut();
+        }, 500
+    );
+}
+
+function fadeOut() {
+    document.getElementById("name").style.background = "transparent";
+    document.getElementById("splash").style.background = "transparent";
+    text1.style.color = "var(--color-font-important)";
+    text2.style.color = "var(--color-font-important)";
+    text3.style.color = "var(--color-font-important)";
+}
+
+function goahead() {
+    window.location.href = "index.html";
+}
+function protip() {
+    window.location.href = "https://docs.google.com/gview?url=http://wertywin2353.github.io/protips.docx&embedded=true";
+}
+document.getElementById('qr-share').onclick = function () {
+    document.getElementById('qr-share').style.translate = "-150%";
+}
+function share() {
+    document.getElementById('qr-share').style.translate = "0%";
+}
---
+++ js/main-app.js
@@ -0,0 +1,107 @@
+let searchEditor = document.getElementById('search');
+let GlDate = new Date();
+let searchSysOBJ = document.getElementById('service');
+let hd = document.getElementById('hd');
+let dataOBJ = document.getElementById('DateOBJ');
+let hrs = document.getElementById('hrs');
+let mins = document.getElementById('mins');
+let searchsys = getC('SearchSys');
+let week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+let months=[
+    'Января',
+    'Февраля',
+    'Марта',
+    'Апреля',
+    'Мая',
+    'Июня',
+    'Июля',
+    'Августа',
+    'Сентября',
+    'Октября',
+    'Ноября',
+    'Декабря',
+    ];
+
+
+window.onload = function () {
+    if(searchsys == undefined) {
+        searchSysOBJ.innerHTML = "Google";
+        setC('SearchSys', 'Google');
+    }
+    else {
+        searchSysOBJ.innerHTML = searchsys;
+    }
+    dataOBJ.innerHTML = week[GlDate.getDay()] + ", " + GlDate.getDate() + " " + months[GlDate.getMonth()];
+    startClock();
+}
+
+document.getElementById('qr-share').onclick = function () {
+    document.getElementById('qr-share').style.translate = "-150%";
+}
+function share() {
+    document.getElementById('qr-share').style.translate = "0%";
+}
+
+function changeSys() {
+    if(searchsys == "Google") {
+        searchSysOBJ.innerHTML = "Yandex";
+        setC('SearchSys', 'Yandex');
+        window.location.reload();
+    }
+    else if(searchsys == "Yandex") {
+        searchSysOBJ.innerHTML = "DuckDuckGo";
+        setC('SearchSys', "DuckDuckGo");
+        window.location.reload();
+    }
+    else if(searchsys == "DuckDuckGo") {
+        searchSysOBJ.innerHTML = "Google"
+        setC('SearchSys', "Google");
+        window.location.reload();
+    }
+}
+
+document.addEventListener("keypress", function onEvent(event) {
+    if (event.key === "Enter") {
+        research();
+    }
+});
+
+function startClock() {
+    timerId = setInterval(update, 1000);
+    update();  
+}
+
+function update() {
+    let date = new Date();
+    let hours = date.getHours();
+    if (hours < 10) hours = '0' + hours;
+    hrs.innerHTML = hours;
+
+    let minutes = date.getMinutes();
+    if (minutes < 10) minutes = '0' + minutes;
+    mins.innerHTML = minutes;
+}
+
+function research() {
+    let src = encodeURI(searchEditor.value);
+    if(searchsys == "Google") {
+        window.location.href = "https://www.google.com/search?q=" + src;
+    }
+    else if(searchsys == "Yandex") {
+        window.location.href = "https://www.yandex.ru/search/?text=" + src;
+    }
+    else if(searchsys == "DuckDuckGo") {
+        window.location.href = "https://duckduckgo.com/?q=" + src;
+    }
+}
+
+function hidebar() {
+    hd.style.translate = "-0% -150%";
+}
+function showbar() {
+    hd.style.translate = "-0% -30%";
+}
+
+function about() {
+    window.location.href = "about.html";
+}
